b9607f06efb4692eafb829e5a56d38632e9b51ef
Machine4 <machine4@udlnet-05-108.udl.net>
2017-03-24 18:32:11 +0100

    First Impl, not working

---
+++ reddit.py
@@ -0,0 +1,37 @@
+# -- coding: utf-8 --
+import praw
+from praw.models import MoreComments
+
+i = 1
+output = open('reddit.txt', 'w+')
+
+def printCommentAndReplies(comment, i):
+    if isinstance(comment, MoreComments):
+        for more in comment.comments():
+            pass
+    else:
+        output.write(str(i) + ": " + comment.body.encode('utf-8') + "\n")
+        i += 1
+
+        for reply in comment.replies.replace_more():
+            i = printCommentAndReplies(reply, i)
+    return i
+
+if __name__ == '__main__' :
+
+
+
+    #print reddit.user.me()
+
+    conversation_id = "6187ay"
+    # conversation_id = "618nlp"
+    conversation_url = "https://www.reddit.com/r/news/comments/6187ay/couple_donates_bug_collection_worth_10m_a/"
+
+    submission = reddit.submission(id=conversation_id)
+    # submission = reddit.submission(url=conversation_url)
+
+    top_comments = submission.comments
+
+    for comment in top_comments.replace_more(limit=2):
+         i = printCommentAndReplies(comment, i)
+    print i
